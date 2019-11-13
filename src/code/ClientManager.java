755760a446219218a024b1b98e9b133edecd26d8
Natnael Mulugeta <natnaelmu.07@gmail.com>
2019-11-14 04:15:23 +1100

    client manager and command added

---
+++ src/code/ClientManager.java
@@ -0,0 +1,135 @@
+package app.code;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.List;
+
+public class ClientManager {
+
+    private  Socket socket;
+    private  int PORT=7789;
+    private  String ADDRESS="127.0.0.1";
+    private  ObjectOutputStream outputStream;
+    private  ObjectInputStream inputStream;
+    private  boolean connected=false;
+
+    public ClientManager(){}
+
+    public ClientManager(int PORT,String address){
+        this.PORT=PORT;
+        this.ADDRESS=address;
+    }
+
+    public String initialize_connection(){
+        try {
+            this.socket=new Socket(this.ADDRESS,this.PORT);
+            this.outputStream=new ObjectOutputStream(this.socket.getOutputStream());
+            this.inputStream=new ObjectInputStream(this.socket.getInputStream());
+            this.connected=true;
+
+            return "Connection established successfully.";
+        } catch (Exception e) {
+            this.connected=false;
+            return "Unable to connect to the server : "+e.getMessage();
+        }
+    }
+
+    public Word getWord(String key){
+        Word word=null;
+        try {
+            Message message=new Message(Commads.GETWORD,key);
+            this.outputStream.writeObject(message);
+            this.outputStream.flush();
+            word=(Word)((Message) this.inputStream.readObject()).data;
+        } catch (Exception e) {
+            if(!this.connected){
+                this.initialize_connection();
+            }else{
+                e.printStackTrace();
+            }
+        }
+
+        return word;
+    }
+
+    public boolean addWord(Word word){
+        boolean answer=false;
+        try {
+            Message message=new Message(Commads.ADD,word);
+            this.outputStream.writeObject(message);
+            this.outputStream.flush();
+            answer=(boolean)((Message) this.inputStream.readObject()).data;
+        } catch (Exception e) {
+            if(!this.connected){
+                this.initialize_connection();
+            }else{
+                e.printStackTrace();
+            }
+        }
+        return answer;
+    }
+
+    public boolean removeWord(String key){
+        boolean answer=false;
+        try {
+            Message message=new Message(Commads.DELETE,key);
+            this.outputStream.writeObject(message);
+            this.outputStream.flush();
+            answer=(boolean)((Message) this.inputStream.readObject()).data;
+        } catch (Exception e) {
+            if(!this.connected){
+                this.initialize_connection();
+            }else{
+                e.printStackTrace();
+            }
+        }
+        return answer;
+    }
+
+    public boolean replaceWord(Word word){
+        boolean answer=false;
+        try {
+            Message message=new Message(Commads.UPDATE,word);
+            this.outputStream.writeObject(message);
+            this.outputStream.flush();
+            answer=(boolean)((Message) this.inputStream.readObject()).data;
+        } catch (Exception e) {
+            if(!this.connected){
+                this.initialize_connection();
+            }else{
+                e.printStackTrace();
+            }
+        }
+        return answer;
+    }
+
+    public List getSuggestion(String key){
+        List meanings=null;
+        try {
+            Message message=new Message(Commads.SEARCH,key);
+            this.outputStream.writeObject(message);
+            this.outputStream.flush();
+            meanings=(List) ((Message) this.inputStream.readObject()).data;
+        } catch (Exception e) {
+            if(!this.connected){
+                this.initialize_connection();
+            }else{
+                e.printStackTrace();
+            }
+        }
+
+        return meanings;
+    }
+
+    public void exit(){
+        try {
+            this.outputStream.close();
+            this.inputStream.close();
+            this.socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/code/Commads.java
@@ -0,0 +1,5 @@
+package app.code;
+
+public enum Commads {
+    GETWORD,SEARCH,UPDATE,DELETE,ADD,EXIT;
+}
