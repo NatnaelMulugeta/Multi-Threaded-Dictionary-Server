a878bd0b9a6ecb74f13ec7b9b21bb2ee11e81f48
Natnael Mulugeta <natnaelmu.07@gmail.com>
2019-11-18 15:27:03 +1100

    DictionaryServer class adds some staff

--- src/code/DictionaryServer.java
+++ src/code/DictionaryServer.java
@@ -1,22 +1,26 @@
 package app.code;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.*;
 
 public class DictionaryServer {
     private IOManager ioManager;
-    private int PORT;
+    private int PORT=7789;
     private ServerSocket server;
+    private String filename="dictionary.json";
 
-    public DictionaryServer(int port){
+    public DictionaryServer(){}
+    public DictionaryServer(int port, String filename){
         this.PORT=port;
+        this.filename=filename;
     }
 
     public void initialize_server(){
         try{
 
             this.server= new ServerSocket(this.PORT);
-            this.ioManager=new IOManager();
+            this.ioManager=new IOManager(this.filename);
 
         }catch (IOException e) {
             System.out.println(e.getMessage());
@@ -25,16 +29,36 @@ public class DictionaryServer {
 
     public void start_server(){
         this.ioManager.populateDictionary();
+        DictionaryThreadPool dictionaryThreadPool= new DictionaryThreadPool(10);
+        System.out.println("----server started-----");
         while(true){
             try {
-                System.out.println("----server start-----");
                 Socket socket= this.server.accept();
-                new UserHandler(socket,this.ioManager).start();
+                System.out.println("new client at "+socket.getInetAddress()+":"+socket.getPort());
+                ///UserHandler userHandler=new UserHandler(socket,this.ioManager);
+                ///.start();
+                dictionaryThreadPool.execute(new UserHandler(socket,this.ioManager));
             } catch (IOException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
+
+    public static void main(String[] arg) {
+        DictionaryServer server=null;
+        if (arg.length>0){
+            try{
+                server =new DictionaryServer(Integer.parseInt(arg[0]),arg[1]);
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null," Please fill a valid Port number and filename. ");
+                return;
+            }
+        }else{
+            server=new DictionaryServer();
+        }
+        server.initialize_server();
+        server.start_server();
+    }
 
 }
